bf9bf0fa5ff30c130c7217d758dbc735802790e5
Balkiya <balkiyakumisbaevaa.91@gmail.com>
2024-02-26 11:11:10 +0600

    database3

--- bisapp/urls.py
+++ bisapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import homepage
+from .views import homepage,index,books,cart,login,contact
 
 urlpatterns = [
-    path('homepage/', homepage)
+    path('homepage/', homepage),
+    path('index/', index),
+    path('books/', books),
+    path('contact/', contact),
+    path('cart/', cart),
+    path('login/', login)
 ]
--- bisapp/views.py
+++ bisapp/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def books(request):
+    return render(request, 'books.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def cart(request):
+    return render(request, 'cart.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def index(request):
+    return render(request, 'index.html')
